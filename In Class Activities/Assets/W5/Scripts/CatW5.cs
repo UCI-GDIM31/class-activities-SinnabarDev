ae3046b7b9b000ddb7aa5da1c58deba71ea8c976
SinnabarDev <killer580death@yahoo.com>
2025-10-27 20:23:28 -0700

    w5 activity

--- In Class Activities/Assets/W5/Scripts/CatW5.cs
+++ In Class Activities/Assets/W5/Scripts/CatW5.cs
@@ -42,10 +42,13 @@ public class CatW5 : MonoBehaviour
         //      them forwards.
         //
         // MULTIPLY one of your vectors with a certain value to do this. >:)
+        float move = Input.GetAxis("Vertical");
+        if (_flipWSControls)
+            move *= -1f;
 
         Vector3 translation = Vector3.zero;
-
-        translation += new Vector3(0, 0, Input.GetAxis("Forward") * _moveSpeed * Time.deltaTime) ;
+        translation = Vector3.forward * move * _moveSpeed * Time.deltaTime;
+        transform.Translate(translation);
 
         // STEP 1 & 2 ---------------------------------------------------------
 
